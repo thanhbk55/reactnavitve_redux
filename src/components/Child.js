4c897e23c8862f962f5177c13a2df259652062fb
thanhbk55 <dinhduythanh51292@gmail.com>
2018-04-03 16:50:44 +0900

    first commit

---
+++ src/components/Child.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+import {connect} from 'react-redux'
+
+class Child extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    console.log(this.props.counter)
+    return (
+      <View>
+        <Text style={styles.text}>{this.props.counter}</Text>
+      </View>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  counter: state.counter
+})
+
+export default connect(mapStateToProps, null)(Child)
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 100,
+    color:'#000'
+  }
+})
